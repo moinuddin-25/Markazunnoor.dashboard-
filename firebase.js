a4a893e2e89647bb78e717a9ce5acdbe5bde2491
Moinuddin <work.mainuddin@gmail.com>
2026-02-06 10:48:24 +0600

    Update firebase.js
    Signed-off-by: Moinuddin  <work.mainuddin@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 // Replace with your Firebase project config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyCJJbYQmnvcLZ7UKX37u4x3N_L1SwFZuGI",
+    authDomain: "markazunnoor-f5ed7.firebaseapp.com",
+    projectId: "markazunnoor-f5ed7",
+    storageBucket: "markazunnoor-f5ed7.appspot.com",
+    messagingSenderId: "156948689533",
+    appId: "1:156948689533:web:f8898adbcb06f9d1c27714"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
+
 const db = firebase.firestore();
